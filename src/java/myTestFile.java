f1b603d1b5dfcf0995f040ade4f3d021d8096a1a
AliSE1735 <ali.serdan89@gmail.com>
2023-02-28 12:51:02 +0300

    initial commit

--- src/java/myTestFile.java
+++ src/java/myTestFile.java
@@ -1,2 +1,13 @@
 It is a test file
 /Users/Ayşenur/Desktop/CLIproject/src/myTestFile.java
+public class myTestFile{
+
+public static void main(String[] args){
+
+
+System.out.println("Hello CLI project");
+
+}
+
+
+}
